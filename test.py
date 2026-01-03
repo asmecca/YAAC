b133c4bcc92470f8b4734465a64d9aae99c08685
Antonio Smecca <antoniosmecca@gmail.com>
2026-01-03 18:51:42 +0100

    added plot

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import utils
+import YAAC.utils as yaac
 
 filename="/Users/antoniosmecca/Documents/Physics/pdoc_RomaTre/PiPi/Code/DATA/out_smeared/ph0_sm0_Sr0_sm0_ph0_av_all"
 
-corr = utils.read_corr(filename)
-print(corr[0])
+jk_corr = yaac.read_corr(filename)
+yaac.plot_corr(jk_corr,"$at$","$C(t)$")
