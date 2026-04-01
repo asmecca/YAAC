dfa4c115b1bd8db98fa4425c4973cb5d666f3ddb
Antonio Smecca <antoniosmecca@gmail.com>
2026-04-01 17:12:50 +0200

    polsihed minor things

--- YAAC/old_utils.py
+++ YAAC/old_utils.py
@@ -2,14 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import itertools
 
 plt.style.use('./myplot.mplstyle')
 
 class Jackknife:
-    """
-    Modern jackknife estimator for linear and non-linear observables.
-    """
-
+    
     def __init__(self, data, estimator= lambda x:np.mean(x),binsize=None, nbins=None):
         """
         Parameters
@@ -126,41 +124,39 @@ def bin_data(data, binsize=None, nbins=None, axis=0, drop_remainder=True):
 
     return np.moveaxis(x, 0, axis)
 
-def read_corr(filename, tempo=None, from_samples=False, col2=False,
+
+aadef read_corr(filename, tempo=None, from_samples=False, col2=False,
               binsize=None, nbins=None):
-    # Reading Correlator
-    corr=[]
-    t=[]
-    f=open(filename,'r')
-
-    with open(filename) as g:
-        first_line = g.readline().strip('\n')
-        x=first_line.split()
-        cnfg=int(x[0])
-        time=int(x[1])
-        T=int(x[2])
-    g.close()
-    
-    for line in f.readlines():
-        x=line.split()
-        if x[0] != str(cnfg):
-            if col2 == True:
-                corr += [float(x[1])]
-            else:
-                corr += [float(x[0])]
-    f.close()
-
+    """
+    Reading Correlator:
+      Data file must have the following first line:
+      Ncnfg N_t N_t/2
+    """
+    corr = []
+ 
+    with open(filename, 'r') as f:
+        first_line = f.readline().strip('\n')
+        x = first_line.split()
+        cnfg = int(x[0])
+        time = int(x[1])
+        T = int(x[2])
+ 
+        for line in f:
+            x = line.split()
+            if x[0] != str(cnfg):
+                corr += [float(x[1])] if col2 else [float(x[0])]
+ 
     if tempo is not None:
         time=tempo
-
+ 
     C = np.zeros((cnfg,time),dtype=float)
     for i in range(0,cnfg):
         for t in range(0,time):
             C[i][t] = corr[i*time + t]
-
+ 
     if (binsize is not None) or (nbins is not None):
         C = bin_data(C, binsize=binsize, nbins=nbins, axis=0)
-
+ 
     t_C = C.T
     jk_corr = [None]*time
     if from_samples is True:
@@ -171,27 +167,20 @@ def read_corr(filename, tempo=None, from_samples=False, col2=False,
             jk_corr[t] = Jackknife(t_C[t])
     return jk_corr
 
-def plot_corr(corr,xlabel,ylabel,ylim=None,yscale=None,data_label=None,color='blue',marker='o',ncol=1,save=None,hline=None,vline=None):
-    for t in range(0,len(corr)):
+def plot_corr(corr, xlabel, ylabel, ylim=None, yscale=None, data_label=None, color='blue', marker='o', ncol=1, save=None, hline=None, hlabel=None, vline=None, vlabel=None):
+    # plots jackknife correlator
+    for t in range(len(corr)):
         if corr[t] is not None:
-            if yscale == "log":
-                if t==0 and data_label is not None:
-                    plt.errorbar(x=t,y=corr[t].mean,yerr=corr[t].std,color=color,fmt=marker,label=data_label)
-                else:
-                    plt.errorbar(x=t,y=corr[t].mean,yerr=corr[t].std,color=color,fmt=marker)
-            else:
-                if t==0 and data_label is not None:
-                    plt.errorbar(x=t,y=corr[t].mean,yerr=corr[t].std,color=color,fmt=marker,label=data_label)
-                else:
-                    plt.errorbar(x=t,y=corr[t].mean,yerr=corr[t].std,color=color,fmt=marker)                
+            label = data_label if t == 0 else None
+            plt.errorbar(x=t, y=corr[t].mean, yerr=corr[t].std, color=color, fmt=marker, label=label)
     if data_label is not None:
-        plt.legend(loc='best',ncol=ncol)
+        plt.legend(loc='best', ncol=ncol)
     plt.ylabel(ylabel)
     plt.xlabel(xlabel)
     if hline is not None:
-        plt.axhline(y=hline,color='black',ls='--',label=hlabel)
+        plt.axhline(y=hline, color='black', ls='--', label=hlabel)
     if vline is not None:
-        plt.axvline(x=vline,color='black',ls='--',label=vlabel)
+        plt.axvline(x=vline, color='black', ls='--', label=vlabel)
     if ylim is not None:
         y_i, y_f = ylim
         plt.ylim(y_i,y_f)    
@@ -202,7 +191,9 @@ def plot_corr(corr,xlabel,ylabel,ylim=None,yscale=None,data_label=None,color='bl
         fig.savefig(save)
     plt.show()
 
+
 def plot_multi_corr(list_corr,xlabel,ylabel,ylim=None,yscale=None,list_label=None,ncol=1,save=None,x_offset=None,hline=None,herr=None,hlabel=None,vline=None,verr=None,vlabel=None):
+    # plots many correlators for comparisons
     plt.ylabel(ylabel)
     plt.xlabel(xlabel)
     if hline is not None:
@@ -231,26 +222,17 @@ def plot_multi_corr(list_corr,xlabel,ylabel,ylim=None,yscale=None,list_label=Non
             data_label = list_label[i]
         else:
             data_label = None
-        for t in range(0,len(corr)):
+        for t in range(len(corr)):
             if corr[t] is not None and corr[t] is not np.nan:
-                if yscale == "log":
-                    if t==0 and data_label is not None:
-                        plt.errorbar(x=t+i*offset,y=corr[t].mean,yerr=corr[t].std,color=color,fmt=marker,label=data_label)
-                    else:
-                        plt.errorbar(x=t+i*offset,y=corr[t].mean,yerr=corr[t].std,color=color,fmt=marker)
-                else:
-                    if t==0 and data_label is not None:
-                        plt.errorbar(x=t+i*offset,y=corr[t].mean,yerr=corr[t].std,color=color,fmt=marker,label=data_label)
-                    else:
-                        plt.errorbar(x=t+i*offset,y=corr[t].mean,yerr=corr[t].std,color=color,fmt=marker)                    
+                label = data_label if t == 0 else None
+                plt.errorbar(x=t + i * offset, y=corr[t].mean, yerr=corr[t].std, color=color, fmt=marker, label=label)
         if data_label is not None:
-            plt.legend(loc='best',ncol=ncol)
+            plt.legend(loc='best', ncol=ncol)
     if save is not None:
         fig=plt.gcf()
         fig.savefig(save)
-    plt.show()
+    plt.show()    
     
-
 def jackknife_covariance(jk):
     """
     Compute jackknife variance / covariance.
@@ -285,7 +267,7 @@ def jackknife_covariance(jk):
     jk_list = list(jk)
     Nt = len(jk_list)
 
-    # Consistency check (important!)
+    # Consistency check
     N = jk_list[0].N
     for j in jk_list:
         if j.N != N:
@@ -328,6 +310,19 @@ def jack_add(jk1,jk2):
 
 
 def add_corrs(corr1,corr2):
+    """
+    Add two Jackknife correlators sample-by-sample.
+
+    Parameters
+    ----------
+    corr1, corr2 : Jackknife
+        Objects with identical N and compatible shapes
+
+    Returns
+    -------
+    Jackknife
+        New jackknifed object representing the product
+    """    
     if len(corr1) != len(corr2):
         raise ValueError("Correlators must have the same length")
 
@@ -358,6 +353,20 @@ def jack_add_d(jk1,d):
 
 
 def add_corrs_d(corr1,d):
+    """
+    Add a Jackknife correlator and a double sample-by-sample.
+
+    Parameters
+    ----------
+    corr1 : Jackknife
+        Objects with identical N and compatible shapes    
+    d : float
+
+    Returns
+    -------
+    Jackknife
+        New jackknifed object representing the product
+    """    
     res=[None]*len(corr1)
     for t in range(len(corr1)):
         res[t] = jack_add_d(corr1[t],d)
@@ -426,6 +435,20 @@ def jack_div_d(jk1,d):
     return Jackknife.from_samples(samples)
 
 def multiply_corrs(corr1,corr2):
+    """
+    Multiply two Jackknife correlators sample-by-sample.
+
+    Parameters
+    ----------
+    corr1, corr2 : Jackknife
+        Objects with identical N and compatible shapes
+
+    Returns
+    -------
+    Jackknife
+        New jackknifed object representing the product
+    """
+    
     if len(corr1) != len(corr2):
         raise ValueError("Correlators must have the same length")
 
@@ -436,6 +459,20 @@ def multiply_corrs(corr1,corr2):
     return res
 
 def multiply_corr_d(corr1,d):
+    """
+    Multiply a Jackknife correlators and a double sample-by-sample.
+
+    Parameters
+    ----------
+    corr1 : Jackknife
+        Objects with identical N and compatible shapes
+    d: float
+
+    Returns
+    -------
+    Jackknife
+        New jackknifed object representing the product
+    """    
     if isinstance(d,float) is False:
         raise ValueError("d is not a float")
 
@@ -473,6 +510,20 @@ def jack_div(jk1,jk2,check_zero=True):
     return Jackknife.from_samples(samples)
 
 def divide_corrs(corr1,corr2):
+    """
+    Divide two Jackknife correlators sample-by-sample.
+
+    Parameters
+    ----------
+    corr1, corr2 : Jackknife
+        Objects with identical N and compatible shapes
+
+    Returns
+    -------
+    Jackknife
+        New jackknifed object representing the product
+    """
+    
     if len(corr1) != len(corr2):
         raise ValueError("Correlators must have the same length")
 
@@ -483,6 +534,21 @@ def divide_corrs(corr1,corr2):
     return res
 
 def divide_corr_d(corr1,d):
+    """
+    Divide a Jackknife correlators and a double sample-by-sample.
+
+    Parameters
+    ----------
+    corr1 : Jackknife
+        Objects with identical N and compatible shapes
+    d: float
+
+    Returns
+    -------
+    Jackknife
+        New jackknifed object representing the product
+    """    
+    
     if isinstance(d,float) is False:
         raise ValueError("d is not a float")
 
@@ -620,6 +686,7 @@ def effective_mass(jack_C, method="cosh"):
     method : str
         Effective mass definition.
         Currently supported:
+        - "cosh": solves C(t)/C(t+1) = cosh(m * (t-T/2)) / cosh(m * (t+1 - T/2))
         - "log" : log(C(t) / C(t+1))
 
     Returns
@@ -638,7 +705,7 @@ def effective_mass(jack_C, method="cosh"):
         jk_ratio = jack_div(jack_C[t], jack_C[t + 1])
 
         if method == "log":
-            # Apply log replica-by-replica
+            # Apply log sample-by-sample
             samples = np.log(jk_ratio.jk_samples)
 
         elif method == "cosh":
@@ -673,6 +740,15 @@ def effective_mass(jack_C, method="cosh"):
     return jack_meff
 
 def fit_effective_mass(jack_C,fit_range=None):
+    """
+    fits effective mass jackknife correlator to a constant
+    
+    Returns
+    ---------
+        E: fitted energy
+        chi2: correlated chi^2/d.o.f.    
+    """
+    
     def cnst_func(x,a):
         return a
     
@@ -783,9 +859,6 @@ def jackknife_fit(corrs, x, fit_func, p0, fit_range=None, correlated=False, cov=
 
         params_jack[i] = popt
         chi2_red[i] = chi2 / dof
-
-    #fit_params.append(params_jack)
-    #chi2_red_median.append(np.median(chi2_red))
 
     return params_jack, np.median(chi2_red)
     
